7328a81169af3466ebf508e9677543b1fc4c92dd
S-Breslin <b.creative3d@outlook.com>
2023-08-10 19:09:47 +0100

    refined landing page

--- main.js
+++ main.js
@@ -2,15 +2,26 @@ let logButton = document.getElementById("login");
 let imgContainer = document.getElementById("background");
 let content = document.getElementById("content");
 
-
+let loadHome = () => {
+    window.location.href = "./home.html";
+};
 
 let showImage = () => {
+    let tl = gsap.timeline();
     content.style.visibility = "hidden";
-    gsap.to("#blur", {
+    tl.to("#blur", {
         opacity: 0,
-        duration: 5, ease: "slow(0.3, 2)"
+        duration: 3, ease: "slow(0.3, 2)", 
+    })
+        .to("#type", {text: "Welcome...", ease:"none", duration: 2.5
+    })
+    .to("#type", {
+        opacity: 0,
+        delay: 1,
+        duration: 2, 
+        ease: "slow(0.3, 2)", 
+        onComplete: loadHome
     });
-    window.location.href = "./home.html";
 
 }
 

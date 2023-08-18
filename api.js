2b5ed758038a5ad1954ed5404a7b7e796be51064
S-Breslin <b.creative3d@outlook.com>
2023-08-18 14:42:57 +0100

    RSVP - Sheets integration

---
+++ api.js
@@ -0,0 +1,22 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxPi-1b1-ohrTAOqg10ukBV_dhxc2_hj_bsCnrL4pTGzZOwy5v0SOcocnpu9sruqDHXrA/exec';
+  const form = document.forms['rsvp'];
+  let background = document.getElementById('rsvp');
+  let formContent = document.getElementById('rsvp_content');
+  let thanks = document.getElementById('thanks');
+
+  const success = () => {
+    background.style.backgroundImage = 'url("./images/Success.jpeg")';
+    background.style.backgroundSize = 'cover';
+    formContent.style.display = 'none';
+    form.style.display = 'none';    
+    thanks.style.display = 'flex';
+    console.log("Success")
+  };
+
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => success())
+      .catch(error => console.error('Error!', error.message))
+  });
